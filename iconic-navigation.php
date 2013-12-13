6118ad37681a5ce7b7fb833732c48389734da05f
indybook <indybook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 13:30:14 +0000

    tagging again ver 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/iconic-navigation/trunk@821093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iconic-navigation.php
+++ iconic-navigation.php
@@ -3,12 +3,12 @@
 Plugin Name: Iconic Navigation for Wordpress
 Plugin URI: http://cadros.eu/
 Description: Upload image icon to a menu item or chose from over 1400 ready font icons. Add custom style. Set a menu display options for smaller screens
-Version: 1.1
+Version: 1.1.2
 Author: Cadros
 Author URI: http://cadros.eu/
 Tags: menu, backend, navigation, nav, icon, flat icon, image menu, font icons
 Requires at least: 3.5
-Tested up to: 3.6.1
+Tested up to: 3.8
 Text Domain: iconic_navigation
 Domain Path: 
 License: GPLv2 or later
